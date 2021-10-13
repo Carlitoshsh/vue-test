ca2b7e36d3d7afcf780df2394bbaa872463ce162
Carlos Gómez <caragomezher@unal.edu.co>
2021-10-13 01:24:00 +0000

    Prueba en vivo

---
+++ my_fe/src/components/Header.vue
@@ -0,0 +1,42 @@
+<template>
+  <header>
+    <h2>Hola mundo</h2>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <p v-if="myVariable">Hola a todos!</p>
+    <p>Nombre: {{ name }} {{ lastname }}</p>
+    <p>Edad: {{ age }}</p>
+    <input v-model="job" placeholder="Trabajo" />
+    <hr />
+
+    <div>
+      <input type="checkbox" v-model="myVariable" />
+      <label>Mostrar mensaje</label>
+    </div>
+    <hr />
+    <p>Trabajo ingresado: {{ job }}</p>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  data: function () {
+    return {
+      myVariable: false,
+      name: "Carlos",
+      age: 26,
+      lastname: "Gomez",
+      job: "",
+    };
+  },
+};
+</script>
+
+<style scoped>
+#nav {
+  background: rgb(151, 176, 243);
+}
+</style>
---
+++ my_fe/src/views/About.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Et quae eaque
+      excepturi laborum labore totam sit nemo libero, numquam delectus?
+      Assumenda quae maxime ab dignissimos non quidem laborum quos magni.
+    </p>
+  </div>
+</template>
