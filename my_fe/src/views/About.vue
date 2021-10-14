c8d318c198d8b24885199b2cc58a64ae6a0f720e
Carlos Gomez <caragomezher@unal.edu.co>
2021-10-13 20:23:22 -0500

    Ejemplo disenio responsive

--- my_fe/src/views/About.vue
+++ my_fe/src/views/About.vue
@@ -1,10 +1,57 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Et quae eaque
-      excepturi laborum labore totam sit nemo libero, numquam delectus?
-      Assumenda quae maxime ab dignissimos non quidem laborum quos magni.
-    </p>
+    <div class="cuadro verde">
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum porro
+        ducimus voluptates dolores ullam nulla nisi consectetur maxime impedit
+        asperiores, debitis sequi, iste, architecto possimus eos dolore
+        pariatur! Voluptas, perspiciatis!
+      </p>
+    </div>
+    <div class="cuadro azul">
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum porro
+        ducimus voluptates dolores ullam nulla nisi consectetur maxime impedit
+        asperiores, debitis sequi, iste, architecto possimus eos dolore
+        pariatur! Voluptas, perspiciatis!
+      </p>
+    </div>
   </div>
 </template>
+
+<style scoped>
+.about {
+  display: flex;
+  justify-content: center;
+  gap: 2rem;
+  padding: 1rem 0.5rem;
+  background-color: lightyellow;
+}
+
+.cuadro {
+  padding: 2rem;
+  border: 1px solid gray;
+  border-radius: 3px;
+  width: 300px;
+}
+
+.verde {
+  background: rgb(177, 248, 177);
+}
+
+.azul {
+  background: rgb(132, 135, 175);
+}
+
+@media only screen and (max-width: 600px) {
+  .about {
+    background-color: lightblue;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .cuadro {
+    width: 200px;
+  }
+}
+</style>
